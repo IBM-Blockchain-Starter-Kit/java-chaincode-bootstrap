c9c2379e65de7338c20455fc632d4f06220a2284
James Taylor <jamest@uk.ibm.com>
2019-05-24 15:20:08 +0100

    Add generated chaincode
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

---
+++ src/main/java/org/example/Chaincode.java
@@ -0,0 +1,30 @@
+/*
+ * SPDX-License-Identifier: Apache License 2.0
+ */
+
+package org.example;
+
+import java.util.List;
+
+import org.hyperledger.fabric.shim.ChaincodeBase;
+import org.hyperledger.fabric.shim.ChaincodeStub;
+
+public class Chaincode extends ChaincodeBase {
+
+    @Override
+    public Response init(ChaincodeStub stub) {
+        String fcn = stub.getFunction();
+        List<String> params = stub.getParameters();
+        System.out.printf("init() %s %s\n", fcn, params.toArray());
+        return ChaincodeBase.newSuccessResponse();
+    }
+
+    @Override
+    public Response invoke(ChaincodeStub stub) {
+        String fcn = stub.getFunction();
+        List<String> params = stub.getParameters();
+        System.out.printf("invoke() %s %s\n", fcn, params.toArray());
+        return ChaincodeBase.newSuccessResponse();
+    }
+
+}
---
+++ src/main/java/org/example/Start.java
@@ -0,0 +1,13 @@
+/*
+ * SPDX-License-Identifier: Apache License 2.0
+ */
+
+package org.example;
+
+public class Start {
+
+    public static void main(String[] args) {
+        new Chaincode().start(args);
+    }
+
+}
